1108ffff6d241675a59b8834140636b4f4be17a4
Christian <crisxian@users.noreply.github.com>
2015-04-02 18:39:49 -0700

    stringifyJSON function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -7,4 +7,44 @@
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
   // your code goes here
+  if(!obj && obj !== 0) return obj + '';
+
+  switch(typeof obj){
+  	case 'undefined' || 'function':
+  		return;
+	case 'string':
+		return '"'+obj+'"';
+	case 'object':
+		if(Array.isArray(obj)) {
+			return stringifyArray(obj);
+		}
+		else {
+			return stringifyObject(obj);
+		}
+	default:
+		return obj.toString(); 
+  }
+
+  function stringifyArray(arr, result) {
+  	var result = result || [];
+  	var copy = arr.slice();
+
+  	if(!arr.length){
+  		return '[' + result.join(',') + ']';
+  	}
+  	var value = copy.shift();
+  	result.push(stringifyJSON(value));
+  	return stringifyArray(copy, result);
+  }
+
+  function stringifyObject(obj) {
+  	var result = [];
+  	for(var key in obj) {
+  		if(typeof obj[key] !== 'function' && typeof obj[key] !== 'undefined') {
+  			result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+  		}
+  	}
+
+  	return '{'+ result.join(',') + '}';
+  }
 };
